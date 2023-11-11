58e3b903b24b486c3a1f276295813a0885ffc7b6
danny6200 <ugookorodaniel620@gmail.com>
2023-11-12 00:12:43 +0100

    Format: test files based on PEP style

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -18,49 +18,60 @@ class TestAmenityInstantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Amenity class."""
 
     def test_no_args_instantiates(self):
+        """ Test no argument instantiation """
         self.assertEqual(Amenity, type(Amenity()))
 
     def test_new_instance_stored_in_objects(self):
+        """ Test new instance stored in object """
         self.assertIn(Amenity(), models.storage.all().values())
 
     def test_id_is_public_str(self):
+        """ Tests id """
         self.assertEqual(str, type(Amenity().id))
 
     def test_created_at_is_public_datetime(self):
+        """ Tests created_at """
         self.assertEqual(datetime, type(Amenity().created_at))
 
     def test_updated_at_is_public_datetime(self):
+        """ Tests updated_at """
         self.assertEqual(datetime, type(Amenity().updated_at))
 
     def test_name_is_public_class_attribute(self):
+        """ Tests name """
         amenity_instance = Amenity()
         self.assertEqual(str, type(Amenity.name))
         self.assertIn("name", dir(Amenity()))
         self.assertNotIn("name", amenity_instance.__dict__)
 
     def test_two_amenities_unique_ids(self):
+        """ Test that id is unique """
         amenity1 = Amenity()
         amenity2 = Amenity()
         self.assertNotEqual(amenity1.id, amenity2.id)
 
     def test_two_amenities_different_created_at(self):
+        """ Test that created_at is unique """
         amenity1 = Amenity()
         sleep(0.05)
         amenity2 = Amenity()
         self.assertLess(amenity1.created_at, amenity2.created_at)
 
     def test_two_amenities_different_updated_at(self):
+        """ Test that updated_at is unique"""
         amenity1 = Amenity()
         sleep(0.05)
         amenity2 = Amenity()
         self.assertLess(amenity1.updated_at, amenity2.updated_at)
 
     def test_str_representation(self):
+        """ Tests __str__ """
         current_datetime = datetime.today()
         current_datetime_repr = repr(current_datetime)
         amenity_instance = Amenity()
         amenity_instance.id = "123456"
-        amenity_instance.created_at = amenity_instance.updated_at = current_datetime
+        amenity_instance.updated_at = current_datetime
+        amenity_instance.created_at = current_datetime
         amenity_str = amenity_instance.__str__()
         self.assertIn("[Amenity] (123456)", amenity_str)
         self.assertIn("'id': '123456'", amenity_str)
@@ -68,6 +79,7 @@ class TestAmenityInstantiation(unittest.TestCase):
         self.assertIn("'updated_at': " + current_datetime_repr, amenity_str)
 
     def test_args_unused(self):
+        """ Test unused args """
         amenity_instance = Amenity(None)
         self.assertNotIn(None, amenity_instance.__dict__.values())
 
@@ -75,12 +87,14 @@ class TestAmenityInstantiation(unittest.TestCase):
         """Instantiation with kwargs test method"""
         current_datetime = datetime.today()
         current_datetime_iso = current_datetime.isoformat()
-        amenity_instance = Amenity(id="345", created_at=current_datetime_iso, updated_at=current_datetime_iso)
+        amenity_instance = Amenity(id="345", created_at=current_datetime_iso,
+                                   updated_at=current_datetime_iso)
         self.assertEqual(amenity_instance.id, "345")
         self.assertEqual(amenity_instance.created_at, current_datetime)
         self.assertEqual(amenity_instance.updated_at, current_datetime)
 
     def test_instantiation_with_None_kwargs(self):
+        """ Tests instantiation  with none kwargs"""
         with self.assertRaises(TypeError):
             Amenity(id=None, created_at=None, updated_at=None)
 
@@ -90,12 +104,14 @@ class TestAmenitySave(unittest.TestCase):
 
     @classmethod
     def setUp(self):
+        """ set up  """
         try:
             os.rename("vault.json", "tmp")
         except IOError:
             pass
 
     def tearDown(self):
+        """ tear down or clean up """
         try:
             os.remove("vault.json")
         except IOError:
@@ -106,6 +122,7 @@ class TestAmenitySave(unittest.TestCase):
             pass
 
     def test_one_save(self):
+        """ Test save method """
         amenity_instance = Amenity()
         sleep(0.05)
         first_updated_at = amenity_instance.updated_at
@@ -113,6 +130,7 @@ class TestAmenitySave(unittest.TestCase):
         self.assertLess(first_updated_at, amenity_instance.updated_at)
 
     def test_two_saves(self):
+        """ Test two save method """
         amenity_instance = Amenity()
         sleep(0.05)
         first_updated_at = amenity_instance.updated_at
@@ -124,11 +142,13 @@ class TestAmenitySave(unittest.TestCase):
         self.assertLess(second_updated_at, amenity_instance.updated_at)
 
     def test_save_with_arg(self):
+        """ Test save method with arg """
         amenity_instance = Amenity()
         with self.assertRaises(TypeError):
             amenity_instance.save(None)
 
     def test_save_updates_file(self):
+        """ Test save update file """
         amenity_instance = Amenity()
         amenity_instance.save()
         amenity_id = "Amenity." + amenity_instance.id
@@ -140,9 +160,11 @@ class TestAmenityToDict(unittest.TestCase):
     """Unittests for testing to_dict method of the Amenity class."""
 
     def test_to_dict_type(self):
+        """ Test to_dict method """
         self.assertTrue(dict, type(Amenity().to_dict()))
 
     def test_to_dict_contains_correct_keys(self):
+        """ Test to_dict contains ccorrect keys"""
         amenity_instance = Amenity()
         self.assertIn("id", amenity_instance.to_dict())
         self.assertIn("created_at", amenity_instance.to_dict())
@@ -150,6 +172,7 @@ class TestAmenityToDict(unittest.TestCase):
         self.assertIn("__class__", amenity_instance.to_dict())
 
     def test_to_dict_contains_added_attributes(self):
+        """ Test to_dict contains added attributes"""
         amenity_instance = Amenity()
         amenity_instance.middle_name = "Holberton"
         amenity_instance.my_number = 98
@@ -157,6 +180,7 @@ class TestAmenityToDict(unittest.TestCase):
         self.assertIn("my_number", amenity_instance.to_dict())
 
     def test_to_dict_datetime_attributes_are_strs(self):
+        """ Test to_dict datetime attributes are strings"""
         amenity_instance = Amenity()
         amenity_dict = amenity_instance.to_dict()
         self.assertEqual(str, type(amenity_dict["id"]))
@@ -164,6 +188,7 @@ class TestAmenityToDict(unittest.TestCase):
         self.assertEqual(str, type(amenity_dict["updated_at"]))
 
     def test_to_dict_output(self):
+        """ Test to_dict output"""
         dt = datetime.today()
         amenity_instance = Amenity()
         amenity_instance.id = "987456"
@@ -177,10 +202,12 @@ class TestAmenityToDict(unittest.TestCase):
         self.assertDictEqual(amenity_instance.to_dict(), tdict)
 
     def test_contrast_to_dict_dunder_dict(self):
+        """ Test constrast to_dict"""
         amenity_in = Amenity()
         self.assertNotEqual(amenity_in.to_dict(), amenity_in.__dict__)
 
     def test_to_dict_with_arg(self):
+        """ Test to_dict with args """
         amenity_instance = Amenity()
         with self.assertRaises(TypeError):
             amenity_instance.to_dict(None)
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -20,36 +20,34 @@ class TestReviewClass(unittest.TestCase):
         """ Set up a Review Class instance """
         self.review_instance = Review()
 
-   
     def test_attributes_inherited_from_BaseModel(self):
         """ Tests that state has BaseModel attributes"""
-        self.assertTrue(hasattr(self.review_instance,"id"))
-        self.assertTrue(hasattr(self.review_instance,"created_at"))
-        self.assertTrue(hasattr(self.review_instance,"updated_at"))
-    
+        self.assertTrue(hasattr(self.review_instance, "id"))
+        self.assertTrue(hasattr(self.review_instance, "created_at"))
+        self.assertTrue(hasattr(self.review_instance, "updated_at"))
+
     def test_default_values(self):
         """ Tests that default value of name set properly """
-        self.assertEqual(self.review_instance.text, "", "default value of text must be empty string")
-        self.assertEqual(self.review_instance.place_id, "", "default value of place_id must be empty string")
-        self.assertEqual(self.review_instance.user_id, "", "default value of user_id must be empty string")
+        self.assertEqual(self.review_instance.text, "")
+        self.assertEqual(self.review_instance.place_id, "")
+        self.assertEqual(self.review_instance.user_id, "")
 
     def test_text_attribute(self):
         """ Tests text attribute """
         self.review_instance.text = "Random text"
-        self.assertEqual(self.review_instance.text, "Random text", "text must be set to value passed")
-    
+        self.assertEqual(self.review_instance.text, "Random text")
+
     def test_created_at_and_updated_at_are_datetime_objects(self):
         """ Test that created_at and updated_at are datetime objects"""
         self.assertIsInstance(self.review_instance.created_at, datetime)
         self.assertIsInstance(self.review_instance.updated_at, datetime)
-
 
     def test_save_method_inherited_from_BaseModel(self):
         """ Tests that save updates 'updated_at' """
         prev_updated_at = self.review_instance.updated_at
         self.review_instance.save()
         curr_updated_at = self.review_instance.updated_at
-        self.assertNotEqual(prev_updated_at, curr_updated_at, "save method should update updated_at")
+        self.assertNotEqual(prev_updated_at, curr_updated_at)
 
     def test_to_dict_method_inherited_from_BaseModel(self):
         """ Tests that to_dict returns a dictionary representation of
@@ -59,12 +57,12 @@ class TestReviewClass(unittest.TestCase):
         created_at_str = dict_rep["created_at"]
         updated_at_str = dict_rep["updated_at"]
 
-        self.assertIsInstance(dict_rep, dict, "to_dict should return a dictionary")
-        self.assertIn("__class__", dict_rep, "dict_rep should contain '__class__' key")
-        self.assertEqual(dict_rep["__class__"], "Review", "'__class__' should be the class name")
+        self.assertIsInstance(dict_rep, dict)
+        self.assertIn("__class__", dict_rep)
+        self.assertEqual(dict_rep["__class__"], "Review")
 
-        self.assertIsInstance(created_at_str, str, "created_at should be a string")
-        self.assertIsInstance(updated_at_str, str, "updated_at should be a string")
+        self.assertIsInstance(created_at_str, str)
+        self.assertIsInstance(updated_at_str, str)
 
         try:
             datetime.fromisoformat(created_at_str)
@@ -72,6 +70,7 @@ class TestReviewClass(unittest.TestCase):
 
         except ValueError:
             self.fail("created_at and updated_at must be in ISO format")
+
 
 if "__name__" == "__main__":
     unittest.main()
